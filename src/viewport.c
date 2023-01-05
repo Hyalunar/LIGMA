41774fe5204d0944a0af16289f591582de5c0a31
Hyalunar <vegotenksGogeto@gmx.de>
2023-01-05 15:59:59 +0100

    Refactored drawingArea to drawingWidget Made drawingWidget accept events motion and buttonpress

--- src/viewport.c
+++ src/viewport.c
@@ -1,6 +1,7 @@
 #include "viewport.h"
 
-GtkWidget* drawingArea;
+GtkWidget* drawingWidget;
+cairo_surface_t* drawingSurface;
 
 /**
  * @brief Callback for a buttonpress on the drawing viewport
@@ -12,18 +13,31 @@ GtkWidget* drawingArea;
  */
 gboolean Viewport_ButtonPressEvent_Callback(GtkWidget* widget, cairo_t* cr, gpointer data)
 {
-    g_print("Button-Click!\n");
+    printf("Button-Click!\n");
+    return FALSE;
+}
+
+gboolean Viewport_MotionNotifyEvent_Callback(GtkWidget* widget, GdkEventMotion* event, gpointer data)
+{
+    printf("Motion-Notify!\n");
     return FALSE;
 }
 
 void Viewport_Create()
 {
-    drawingArea = gtk_drawing_area_new();
-    gtk_widget_set_size_request(drawingArea, 300, 300);
-    g_signal_connect(G_OBJECT(drawingArea), "button-release-event", G_CALLBACK(Viewport_ButtonPressEvent_Callback), NULL);
+    drawingWidget = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawingWidget, 300, 300);
+    gtk_widget_set_events(drawingWidget, gtk_widget_get_events(drawingWidget) | GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK);
+    g_signal_connect(G_OBJECT(drawingWidget), "button-release-event", G_CALLBACK(Viewport_ButtonPressEvent_Callback), NULL);
+    g_signal_connect(G_OBJECT(drawingWidget), "motion-notify-event", G_CALLBACK(Viewport_MotionNotifyEvent_Callback), NULL);
+
+    drawingSurface = cairo_image_surface_create(
+        CAIRO_FORMAT_ARGB32, 
+        gtk_widget_get_allocated_height(drawingWidget),
+        gtk_widget_get_allocated_height(drawingWidget));
 }
 
 GtkWidget* Viewport_GetWidget()
 {
-    return drawingArea;
+    return drawingWidget;
 }
