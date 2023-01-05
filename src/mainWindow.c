f46222ccec96d936a243b2f88107a1c3b5699450
Hyalunar <vegotenksGogeto@gmx.de>
2023-01-05 22:20:53 +0100

    Fixed recursive destroy Added Viewport Added Toolbar

--- src/mainWindow.c
+++ src/mainWindow.c
@@ -3,8 +3,10 @@
 #include <gtk/gtk.h>
 
 #include "toolbar.h"
+#include "viewport.h"
 
 GtkWidget* window;
+GtkWidget* layoutBox;
 
 /**
     This acts as the entry point for the GTK-Application.
@@ -18,7 +20,13 @@ void MainWindow_AppActivate(GtkApplication* app, gpointer user_data)
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
     Toolbar_Create();
-    
+    Viewport_Create();
+
+    layoutBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 15);
+    gtk_box_pack_start(GTK_BOX(layoutBox), GTK_WIDGET(Toolbar_GetWidget()), 1, 1, 15U);
+    gtk_box_pack_end(GTK_BOX(layoutBox), GTK_WIDGET(Viewport_GetWidget()), 1, 1, 15U);
+
+    gtk_container_add(GTK_CONTAINER(window), layoutBox);
 
     gtk_widget_show_all(window);
 }
@@ -29,6 +37,6 @@ void MainWindow_AppActivate(GtkApplication* app, gpointer user_data)
 **/
 void MainWindow_AppShutdown(GtkApplication* app, gpointer user_data)
 {
-    Toolbar_Destroy();
+    gtk_widget_destroy(GTK_WIDGET(window));
     printf("Bye!\n");
 }
