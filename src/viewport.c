5f93c3c33b6fb9b0ca8d5e7221c2a5e37ac7cee3
Hyalunar <vegotenksGogeto@gmx.de>
2023-01-05 22:22:02 +0100

    Created viewport.h Removed unused Viewport_Destroy

--- src/viewport.c
+++ src/viewport.c
@@ -1,13 +1,29 @@
-#include <gtk/gtk.h>
+#include "viewport.h"
 
 GtkWidget* drawingArea;
+
+/**
+ * @brief Callback for a buttonpress on the drawing viewport
+ * 
+ * @param widget Widget generating the callback
+ * @param cr cairo-stuff
+ * @param data data passed at signal registrations
+ * @return FALSE
+ */
+gboolean Viewport_ButtonPressEvent_Callback(GtkWidget* widget, cairo_t* cr, gpointer data)
+{
+    g_print("Button-Click!\n");
+    return FALSE;
+}
 
 void Viewport_Create()
 {
     drawingArea = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawingArea, 300, 300);
+    g_signal_connect(G_OBJECT(drawingArea), "button-release-event", G_CALLBACK(Viewport_ButtonPressEvent_Callback), NULL);
 }
 
-void Viewport_Destroy()
+GtkWidget* Viewport_GetWidget()
 {
-    gtk_destroy_widget(drawingArea);
+    return drawingArea;
 }
---
+++ src/viewport.h
@@ -0,0 +1,11 @@
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+
+#include <stdio.h>
+
+#include <gtk/gtk.h>
+
+void Viewport_Create();
+GtkWidget* Viewport_GetWidget();
+
+#endif // VIEWPORT_H
