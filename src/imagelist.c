94371599d2416f6b60aecee28e9c45bec212fd64
Hyalunar <vegotenksGogeto@gmx.de>
2023-01-06 20:22:53 +0100

    Removed not working alternative widget Added unselectable header widget

--- src/imagelist.c
+++ src/imagelist.c
@@ -8,14 +8,21 @@ void Imagelist_Create()
         return;
     }
 
-    Imagelist      = gtk_list_box_new();
+    Imagelist              = gtk_list_box_new();
+
+    GtkWidget* headerRow   = gtk_list_box_row_new();
     
-    GtkWidget* alternateText   = gtk_text_view_new();
-    GtkTextBuffer* buffer      = gtk_text_view_get_buffer(GTK_TEXT_VIEW(alternateText));
-    gtk_text_buffer_set_text(buffer, "No Images", -1);
-    gtk_list_box_set_placeholder(GTK_LIST_BOX(Imagelist), alternateText);
-    // Create a text view that will be shown if no elements are inside the list box
+    GtkWidget* vBox        = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    GtkWidget* headerLabel = gtk_label_new("Open Images");
+    gtk_widget_set_halign(headerLabel, GTK_ALIGN_CENTER);
+    gtk_container_add(GTK_CONTAINER(vBox), headerLabel);
+    gtk_container_add(GTK_CONTAINER(vBox), gtk_separator_new(GTK_ORIENTATION_VERTICAL));
     
+    gtk_list_box_row_set_selectable(GTK_LIST_BOX_ROW(headerRow), FALSE);
+    gtk_list_box_row_set_activatable(GTK_LIST_BOX_ROW(headerRow), FALSE);
+    gtk_container_add(GTK_CONTAINER(headerRow), vBox);
+
+    gtk_list_box_prepend(GTK_LIST_BOX(Imagelist), headerRow);
 }
 
 /// @brief Return the Widget serving as a Imagelist
