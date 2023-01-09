5b2cb6409c28d61807fdcb2378a17171cc312ab1
Hyalunar <vegotenksGogeto@gmx.de>
2023-01-09 18:21:31 +0100

    Moved files

--- src/mainWindow.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H 1
-
-void MainWindow_AppActivate(GtkApplication* app, gpointer user_data);
-void MainWindow_AppShutdown(GtkApplication* app, gpointer user_data);
-
-#endif
--- src/toolbar.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOOLBAR_H
-#define TOOLBAR_H
-
-#include <gtk/gtk.h>
-
-void Toolbar_Create();
-GtkWidget* Toolbar_GetWidget();
-
-#endif // TOOLBAR_H
