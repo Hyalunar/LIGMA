74f0fe7a93b495a2c3f14efdbe5039371ee8b60d
Hyalunar <vegotenksGogeto@gmx.de>
2023-02-08 19:19:35 +0100

    Included PlugSelector

--- src/ui/mainWindow.c
+++ src/ui/mainWindow.c
@@ -7,6 +7,7 @@
 #include "effectstack.h"
 #include "colorchooser.h"
 #include "appmenu.h"
+#include "plugselector.h"
 
 GtkWidget* window;
 GtkWidget* layoutBox;
@@ -26,11 +27,13 @@ void MainWindow_AppActivate(GtkApplication* app, gpointer user_data)
     Effectstack_Create();
     Colorchooser_Create();
     AppMenu_Create();
+    PlugSelector_Create();
 
 
     layoutBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 15);
     gtk_box_pack_start(GTK_BOX(layoutBox), AppMenu_GetWidget(), TRUE, FALSE, 0U);
     gtk_box_pack_start(GTK_BOX(layoutBox), Toolbar_GetWidget(), TRUE, FALSE, 15U);
+    gtk_box_pack_start(GTK_BOX(layoutBox), PlugSelector_GetWidget(), TRUE, FALSE, 15U);
     // Add Toolbar to master box
 
     GtkWidget* lowerBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 15);
