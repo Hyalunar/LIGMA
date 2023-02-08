3944c4bbe59af5fbfddc8d7143637c6b00bcd30b
Sleppo04 <gamedisk2000@gmail.com>
2023-02-08 16:59:30 +0100

    Calling PluginManager_Init now...

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <gtk/gtk.h>
 #include "ui/mainWindow.h"
+#include "logic/plugManager.h"
 
 int main(int argc, char** argv)
 {
     GtkApplication* app;
     int status;
+
+    PluginManager_Init();
 
     app = gtk_application_new("org.ligma", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(MainWindow_AppActivate), NULL);
