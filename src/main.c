03a8c6399c1a57df03c87bb54e0a532d9ab1921c
Hyalunar <vegotenksGogeto@gmx.de>
2023-02-09 16:36:45 +0100

    Added PluginManager ShutDown and Creation

--- src/main.c
+++ src/main.c
@@ -1,16 +1,21 @@
 #include <gtk/gtk.h>
 #include "ui/mainWindow.h"
+#include "logic/plugManager.h"
 
 int main(int argc, char** argv)
 {
     GtkApplication* app;
     int status;
 
+    PluginManager_Create();
+
     app = gtk_application_new("org.ligma", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(MainWindow_AppActivate), NULL);
     g_signal_connect(app, "shutdown", G_CALLBACK(MainWindow_AppShutdown), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
+
+    PluginManager_Destroy();
 
     return status;
 }
