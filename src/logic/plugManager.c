cadaea03eadaffe8e910bd54d52ec38dac85df1c
Hyalunar <vegotenksGogeto@gmx.de>
2023-02-10 19:45:33 +0100

    Function documentation Error checking Function detecting for Plugs Callbacks on unloads implemented unloads Unload also clears plug resources

--- src/logic/plugManager.c
+++ src/logic/plugManager.c
@@ -49,6 +49,9 @@ guint PluginManager_CallbackIndexByFunction(int (*function) (char, plug_t*))
     return index;
 }
 
+/// @brief Iterate over all registered callback functions and call those that match the eventActionMask
+/// @param eventActionMask mask to be matched by a callback
+/// @param plug Plug to be passed as an argument when the callback is called
 void PluginManager_DoCallback(char eventActionMask, plug_t* plug)
 {
     plugcallback_t plugCallback;
@@ -60,11 +63,20 @@ void PluginManager_DoCallback(char eventActionMask, plug_t* plug)
     }
 }
 
+/// @brief Unregister a Plug identified by its function address
+/// @param function callback function address
+/// @return EINVAL: invalid function argument
+/// @return ENOMEDIUM: No callback with the specified attributes could be found
+/// @return EXIT_SUCCESS: Success, get it?
 int PluginManager_UnregisterCallback(int (*function) (char, plug_t*))
 {
-    guint index = PluginManager_CallbackIndexByFunction(function);
-    if (index == callbackArray->len + 1) {
+    if (function == NULL) {
         return EINVAL;
+    }
+
+    guint index = PluginManager_CallbackIndexByFunction(function);
+    if (index >= callbackArray->len) {
+        return ENOMEDIUM;
     }
 
     g_array_remove_index_fast(callbackArray, index);
@@ -72,6 +84,9 @@ int PluginManager_UnregisterCallback(int (*function) (char, plug_t*))
     return EXIT_SUCCESS;
 }
 
+/// @brief Register a callback at the PlugManager to be called when a specified event occurs
+/// @param function callback function
+/// @param actionMask mask including only the actions this plug should be called with
 void PluginManager_RegisterCallback(int (*function) (char, plug_t*), char actionMask)
 {
     plugcallback_t plugCallback;
@@ -87,6 +102,22 @@ void PluginManager_PlugError(char *s)
     puts(s);
 }
 
+int PlugManager_LoadPlugFunctions(plug_t* plug)
+{
+    #ifdef __unix__
+        plug->loadFunction    = dlsym(plug->dll, "Ligma_OnLoad");
+        plug->unloadFunction  = dlsym(plug->dll, "Ligma_OnUnload");
+        plug->infoFunction    = dlsym(plug->dll, "Ligma_GetPlugInfo");
+        plug->processFunction = dlsym(plug->dll, "Ligma_Process");
+    #elif _WIN32
+
+    #endif
+
+    if (plug->loadFunction == NULL || plug->unloadFunction == NULL || plug->infoFunction == NULL || plug->processFunction == NULL) {
+        return ENOMEDIUM;
+    }
+}
+
 /** @brief Load a Plug with a given path
     @param path plugin to load
     @return EINVAL, ENXIO, EXIT_SUCCESS
@@ -98,53 +129,62 @@ int PluginManager_LoadByPath(char *path)
 {
     plug_t plug;
 
-#ifdef __unix__
-    /* funky linux stuff */
+    #ifdef __unix__
+        /* funky linux stuff */
 
-    plug.dll = NULL;
-    plug.dll = dlopen(path, RTLD_NOW);
-    if (plug.dll == NULL)
-    {
-        return EINVAL;
-    }
-    /** Loaded Plug **/
+        plug.dll = NULL;
+        plug.dll = dlopen(path, RTLD_NOW);
+        if (plug.dll == NULL)
+        {
+            return EINVAL;
+        }
+    
+    #elif _WIN32
+        /** sadly windows stuff **/
+        // TODO: Find someone to do this for me
+    #endif
 
-    /** TODO: Make cleanup in return clauses **/
 
-    /** Get info Function **/
-    dlerror(); /** Clear the error **/
-    int (*getPlugInfoFunc)(pluginfo_t *) = dlsym(plug.dll, "Ligma_GetPlugInfo");
-    if (getPlugInfoFunc == NULL)
-    {
-        PluginManager_PlugError(dlerror());
-        return ENXIO;
-    }
-
-    if (getPlugInfoFunc(&(plug.pluginfo)))
-    {
-        PluginManager_PlugError("Failed to load Plugin info");
-        return ECANCELED;
-    }
-
-    dlerror();
-    int (*plugOnLoadFunc)(void) = dlsym(plug.dll, "Ligma_OnLoad");
-    if (plugOnLoadFunc == NULL)
-    {
-        PluginManager_PlugError(dlerror());
-        return ENXIO;
-    }
-
-    if (plugOnLoadFunc())
-    {
-        PluginManager_PlugError("Loading the plug failed.");
-        return ECANCELED;
-    }
-
-#elif _WIN32
-/** sadly windows stuff **/
-// TODO: Find someone to do this for me
-#endif
 
     PluginManager_DoCallback(PLUGACTION_LOAD, &plug);
     g_array_append_val(plugArray, plug);
+}
+
+/// @brief Unload a Plug identified by its name
+/// @param name Name of the Plug to unload
+/// @return ENOMEDIUM: No such plugin was found
+/// @return EINVAL: GString was invalid
+/// @return EXIT_SUCCESS: Operation performed successfully
+int PluginManager_UnloadByName(GString* name)
+{
+    plug_t* plug;
+    guint   plugIndex;
+    for (guint plugIndex = 0; plugIndex < plugArray->len; plugIndex++) {
+        plug = &g_array_index(plugArray, plug_t, plugIndex);
+        GString* plugName = plug->pluginfo.name;
+        if (g_string_equal(name, plugName)) {
+            break;
+        }
+    }
+
+    if (plugIndex < plugArray->len) {
+        return ENOMEDIUM;
+    }
+
+
+    PluginManager_DoCallback(PLUGACTION_UNLOAD, plug);
+
+    #ifdef __unix__
+    dlclose(plug->dll);
+    
+    #elif _WIN32
+
+    #endif
+
+    plug->dll = NULL;
+    g_string_free(plug->pluginfo.about,     TRUE);
+    g_string_free(plug->pluginfo.name,      TRUE);
+    g_string_free(plug->pluginfo.publisher, TRUE);
+    g_array_unref(plug->pluginfo.arguments);
+    return EXIT_SUCCESS;
 }
